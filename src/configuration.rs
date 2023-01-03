5586a9839a7d666677bd96e16a2a9196a23c5d8c
Florin <florin.diaconeasa@gmail.com>
2023-01-03 23:20:39 +0200

    make config loading return any underlying error without wrapping it

---
+++ src/configuration.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use config::{Config, FileFormat};
+use url::Url;
+
+const CONFIG_URL: &str = "service.url";
+const CONFIG_REFRESH_SEC: &str = "service.refresh_ms";
+#[allow(unused)]
+const CONFIG_TWILIO_TOKEN: &str = "";
+#[allow(unused)]
+const CONFIG_TWILIO_PHONE: &str = "refresh_ms";
+
+#[derive(Debug, Clone)]
+pub struct ServiceConfiguration {
+    pub url: String,
+    pub refresh_ms: i64,
+    pub auth_token: String,
+    pub phone_numer: String,
+}
+
+impl ServiceConfiguration {
+    pub fn new(file_path: &str) -> Result<Self, Box<dyn Error>> {
+        let config = Config::builder()
+            .add_source(config::File::new(file_path, FileFormat::Toml))
+            .build()?;
+
+        let service_configuration = Self {
+            url: config.get_string(CONFIG_URL)?,
+            refresh_ms: config.get_int(CONFIG_REFRESH_SEC)?,
+            auth_token: String::from("no token"),
+            phone_numer: String::from("no phone"),
+        };
+
+        Url::parse(&service_configuration.url)?;
+
+        Ok(service_configuration)
+    }
+}
+
+impl Display for ServiceConfiguration {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "url: {}", self.url)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,61 @@
-use std::collections::HashMap;
+extern crate core;
+
 use std::env;
 
 use rss::Channel;
 
-use crate::param_validation::{ArgumentsError, validate_params};
-
-mod param_validation;
+mod configuration;
 
 /**
 URL: https://www.e-distributie.com/content/dam/e-distributie/outages/rss/enel_rss_muntenia.xml
  */
-fn main() -> Result<(), ArgumentsError> {
+fn main() {
     let cli_arg = env::args().nth(1);
-    let url = validate_params(cli_arg)?;
+    let file_path = validate_config_file(cli_arg);
 
-    println!("Input is valid. Starting to parse it.");
+    let config_result = configuration::ServiceConfiguration::new(&file_path);
 
-    let content = reqwest::blocking::get(url)
-        .unwrap()
-        .bytes()
-        .unwrap();
+    match config_result {
+        Err(err) => {
+            panic!("There was an error when loading the configuration: {}", err)
+        }
+        Ok(config) => {
+            println!("Using configuration: {}", config);
 
-    let channel = Channel::read_from(&content[..]).unwrap();
+            let content = reqwest::blocking::get(config.url)
+                .unwrap()
+                .bytes()
+                .unwrap();
 
-    let affected_locations = channel.items.len();
-    println!("Scheduled downtime locations: {}", affected_locations);
+            let channel = Channel::read_from(&content[..]).unwrap();
 
-    if affected_locations > 10 {
-        send_sms(affected_locations);
+            let affected_locations = channel.items.len();
+            println!("Scheduled downtime locations: {}", affected_locations);
+
+            if affected_locations > 10 {
+                send_sms(affected_locations);
+            }
+        }
     }
+}
 
+fn validate_config_file(cli_arg: Option<String>) -> String {
+    match cli_arg {
+        Some(file_path) => {
+            let file_exists = std::path::Path::new(&file_path).exists();
 
-    Ok(())
+            match file_exists {
+                true => {
+                    println!("Using file: {}", &file_path);
+                    file_path
+                }
+                false => {
+                    panic!("Configuration file does not exist!")
+                }
+            }
+        }
+        None => panic!("Configuration file has not been provided.")
+    }
 }
 
 fn send_sms(locations_counter: usize) {
