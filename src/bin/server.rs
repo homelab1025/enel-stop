f7e9eece39061f3eb5c2c7fe5297e86d2de62ffc
Florin <florin.diaconeasa@gmail.com>
2023-07-07 17:06:32 +0300

    simple hello world

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,21 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use log::{info, LevelFilter};
 use simple_logger::SimpleLogger;
+use warp::Filter;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     SimpleLogger::new()
         .env()
         .with_level(LevelFilter::Info)
         .init()
         .unwrap();
 
-    info!("Server starting...")
+    info!("Server starting...");
+
+    let hello = warp::path("api").map(|| "Hello");
+
+    // TODO: understand Into and From traits
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
