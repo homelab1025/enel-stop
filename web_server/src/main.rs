f5c651f964a0df7c5b0c75ab351fde623528fb23
Florin Diaconeasa <florin.diaconeasa@gmail.com>
2025-01-29 21:01:55 +0200

    cleanup web server crate: remove warp and add minimum features for tokio

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,28 +2,13 @@ use std::env;
 
 use log::{info, LevelFilter};
 use simple_logger::SimpleLogger;
-use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    SimpleLogger::new()
-        .env()
-        .with_level(LevelFilter::Info)
-        .init()
-        .unwrap();
+    SimpleLogger::new().env().with_level(LevelFilter::Info).init().unwrap();
 
     let cli_arg = env::args().nth(1).unwrap_or(String::from("8080"));
-    let port = cli_arg.parse::<u16>().unwrap();
+    let _port = cli_arg.parse::<u16>().unwrap();
 
     info!("Server starting...");
-
-    let api = warp::path("api");
-
-    let ping_api = api.and(warp::path("ping")).map(|| "pong");
-    let store_api = api.and(warp::path("store")).map(|| "ok");
-
-    let all_routes = ping_api.or(store_api);
-
-    // TODO: understand Into and From traits
-    warp::serve(all_routes).run(([0, 0, 0, 0], port)).await;
 }
