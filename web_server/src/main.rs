50e0d17da30f1f60660f01d88a2413c1904d4a09
Florin Diaconeasa <florin.diaconeasa@gmail.com>
2025-03-31 22:51:28 +0300

    migrate function kick-off

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,6 +1,8 @@
 use std::{
+    env,
     fs::File,
     io::{BufRead, BufReader, Error},
+    str::FromStr,
 };
 
 use axum::{
@@ -8,12 +10,22 @@ use axum::{
     routing::get,
     Router,
 };
+use common::{
+    configuration::{self, ServiceConfiguration},
+    Record,
+};
 use log::{info, LevelFilter};
+use redis::cmd;
 use simple_logger::SimpleLogger;
 use tokio::{net::TcpListener, runtime};
 
 fn main() {
     SimpleLogger::new().env().with_level(LevelFilter::Info).init().unwrap();
+
+    let config = load_configuration();
+
+    let redis_strig = config.redis_server.expect("Redis server must be configured.");
+    migrate_records(&redis_strig);
 
     let core_count = get_core_count().expect("Could not detect number of cores");
 
@@ -30,6 +42,69 @@ fn main() {
 
         axum::serve(listener, app).await.unwrap();
     });
+}
+
+fn load_configuration() -> ServiceConfiguration {
+    let cli_arg = env::args().nth(1);
+    let config = cli_arg.map(|file_path| configuration::get_configuration(&file_path));
+
+    if config.is_none() {
+        panic!("No configuration has been provided.");
+    }
+
+    let config = config.unwrap();
+    if config.is_err() {
+        panic!("some other config issue: {}", config.unwrap_err());
+    }
+    let config = config.unwrap();
+
+    info!("Configuration: {}", config);
+
+    config
+}
+
+/// Blocking function for migrating the records stored in redis to another structure.
+fn migrate_records(redis_string: &str) {
+    let client = redis::Client::open(redis_string).expect(
+        "Redis client could not be created. Check connection string or remove it if you don't want to store results.",
+    );
+
+    let mut redis_conn = client.get_connection().expect("Could not connect to redis.");
+
+    create_timestamp_sorted_set(&mut redis_conn);
+}
+
+fn create_timestamp_sorted_set(redis_conn: &mut redis::Connection) {
+    let mut cursor = String::from("0");
+    loop {
+        let (next_cursor, keys): (String, Vec<String>) = cmd("SCAN")
+            .arg(cursor)
+            .arg("MATCH")
+            .arg("12*")
+            .arg("COUNT")
+            .arg("1000")
+            .query(redis_conn)
+            .expect("Could not run SCAN command");
+
+        keys.iter().for_each(|key| {
+            info!("KEY {}", key);
+            let record_json: String = cmd("GET").arg(key).query(redis_conn).expect("Could not get the value.");
+            let record: Record = serde_json::from_str(&record_json).expect("Could not deserialize.");
+
+            info!(
+                "Got key {} with time {} so timestamp is {}",
+                record.id,
+                record.date,
+                record.date.and_hms(0, 0, 0).and_utc().timestamp()
+            )
+        });
+
+        if next_cursor == "0" {
+            info!("Wen thru all the keys.");
+            break;
+        }
+        cursor = next_cursor.clone();
+    }
 }
 
 async fn say_hello() -> (http::StatusCode, &'static str) {
