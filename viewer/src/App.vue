0a33d75203156240deaaf1b5771a8bc713f21abf
Florin <florin.diaconeasa@gmail.com>
2025-05-10 14:42:44 +0300

    make request and print the statusˆ

--- viewer/src/App.vue
+++ viewer/src/App.vue
@@ -3,14 +3,18 @@ import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
-  <div class="flex h-[500px]">
-    <div class="w-[200px] bg-blue-300 p-4">
-      <ul class="flex">
-        <RouterLink class="mr-3" to="/">Home</RouterLink>
-        <RouterLink class="mr-3" to="/count">Counter</RouterLink>
-        <RouterLink class="mr-3" to="/about">About</RouterLink>
-      </ul>
-    </div>
+  <div class="flex h-screen">
+    <ul class="menu bg-base-200 rounded-box w-56">
+      <li><a>
+        <RouterLink to="/">Home</RouterLink>
+      </a></li>
+      <li><a>
+        <RouterLink to="/count">Counter</RouterLink>
+      </a></li>
+      <li><a>
+        <RouterLink to="/about">About</RouterLink>
+      </a></li>
+    </ul>
     <div class="flex-1 bg-blue-100 p-4">
       <RouterView/>
     </div>
--- viewer/src/components/TheWelcome.vue
+++ viewer/src/components/TheWelcome.vue
@@ -1,7 +1,19 @@
 <script setup>
+import {ref, onMounted} from 'vue';
 
+const status_number = ref(null);
+
+onMounted(async() =>{
+  const response = await fetch("/aaa");
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  status_number.value = response.status;
+})
 </script>
 
 <template>
-    <div class="text-4xl font-semibold tracking-tight text-gray-600 pt-4">Just a friendly welcome.</div>
+  <div class="text-4xl font-semibold tracking-tight text-gray-600 pt-4">Just a friendly welcome: {{ status_number }}</div>
+
 </template>
