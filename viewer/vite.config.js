3822cc71b8072cf1ca1fdcadd5e33ba32f0d58d5
Florin <florin.diaconeasa@gmail.com>
2025-05-11 11:34:26 +0300

    remove base url

--- viewer/vite.config.js
+++ viewer/vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/web',
+    // base: '/web',
     plugins: [
         vue(),
         vueDevTools(),
