0f22adddbfff1e91b17c8c69be29a0e7de71a10f
Florin <florin.diaconeasa@gmail.com>
2025-05-18 18:30:02 +0300

    separate api module and return results from handlers

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -1,23 +1,14 @@
-use axum::extract::State;
-use axum::{
-    http::{self, StatusCode},
-    routing::get,
-    Router,
-};
+use axum::{routing::get, Router};
 use common::configuration::{self, ServiceConfiguration};
 use log::{info, LevelFilter};
-use redis::aio::ConnectionLike;
-use redis::RedisError;
+use redis::aio::{ConnectionLike};
 use simple_logger::SimpleLogger;
 use std::env;
-use std::ops::Deref;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::{net::TcpListener, runtime};
 use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
-use web_server::migration::sorted_set::SortedSetMigration;
-use web_server::migration::MigrationProcess;
 
 pub mod migration;
 
@@ -63,12 +54,12 @@ fn main() {
         };
 
         let mut app = Router::new()
-            .route( "/api/ping", get(ping))
-            .route("/api/incidents/count", get(count_incidents))
+            .route("/api/ping", get(api::ping))
+            .route("/api/incidents/count", get(api::count_incidents))
             .fallback_service(ServeDir::new("web_assets"))
             .with_state(state);
 
-        if (config.cors_permissive) {
+        if config.cors_permissive {
             app = app.layer(CorsLayer::permissive());
         }
         let addr = format!("0.0.0.0:{}", config.http_port);
@@ -78,27 +69,48 @@ fn main() {
     });
 }
 
-async fn count_incidents<T>(state: State<AppState<T>>) -> (StatusCode, String)
-where
-    T: ConnectionLike + Send + Sync,
-{
-    let mut conn_guard = state.redis_conn.lock().await;
-    let conn = &mut *conn_guard;
-    let counter: Result<u64, RedisError> = redis::cmd("DBSIZE").query_async(conn).await;
+mod api {
+    use crate::AppState;
+    use axum::extract::State;
+    use axum::http::StatusCode;
+    use axum::Json;
+    use redis::aio::ConnectionLike;
+    use redis::RedisError;
+    use serde::Serialize;
+    use std::ops::Deref;
 
-    match counter {
-        Ok(key_count) => (StatusCode::OK, key_count.to_string()),
-        Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()),
+    #[derive(Debug, Serialize, Clone)]
+    pub struct RecordCount {
+        count: u64,
     }
-}
 
-async fn ping<T>(state: State<AppState<T>>) -> (StatusCode, String)
-where
-    T: ConnectionLike + Send + Sync,
-{
-    let a = state.ping_msg.deref();
-    let response = format!("Hello {}!", a);
-    (StatusCode::OK, response)
+    #[derive(Debug, Serialize, Clone)]
+    pub struct Ping {
+        ping: String,
+    }
+
+    pub async fn count_incidents<T>(state: State<AppState<T>>) -> Result<Json<RecordCount>, (StatusCode, String)>
+    where
+        T: ConnectionLike + Send + Sync,
+    {
+        let mut conn_guard = state.redis_conn.lock().await;
+        let conn = &mut *conn_guard;
+        let counter: Result<u64, RedisError> = redis::cmd("DBSIZE").query_async(conn).await;
+
+        match counter {
+            Ok(key_count) => Ok(Json(RecordCount { count: key_count })),
+            Err(err) => Err((StatusCode::INTERNAL_SERVER_ERROR, err.to_string())),
+        }
+    }
+
+    pub async fn ping<T>(state: State<AppState<T>>) -> Result<Json<Ping>, (StatusCode, String)>
+    where
+        T: ConnectionLike + Send + Sync,
+    {
+        let a = state.ping_msg.deref();
+        let response = format!("Hello {}!", a);
+        Ok(Json(Ping { ping: response }))
+    }
 }
 
 fn load_configuration() -> ServiceConfiguration {
