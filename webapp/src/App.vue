38751fd4b9a39c19c79744daa5244d3b417df5df
Florin <florin.diaconeasa@gmail.com>
2025-05-25 22:21:12 +0300

    webapp in TS using vue

---
+++ webapp/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router'
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <ul class="menu bg-base-200 rounded-box w-48">
+      <li>
+        <RouterLink to="/">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/list">Lister</RouterLink>
+      </li>
+    </ul>
+    <div class="flex-1 bg-blue-100 p-4">
+      <RouterView/>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
