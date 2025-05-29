81f23dd119cbc0c8614d7974b0091a6327ad0d76
Florin <florin.diaconeasa@gmail.com>
2025-05-29 12:42:54 +0300

    GH actions jobs share the work directory ONLY if they fall on the same runner

--- api_gen/src/main.rs
+++ api_gen/src/main.rs
@@ -1,10 +1,12 @@
+use std::env;
 use utoipa::OpenApi;
 
 fn main() {
+    let path = env::args().nth(1).expect("Missing path argument");
     let open_api = web_server::api::ApiDoc::openapi();
     match open_api.to_yaml() {
         Ok(yaml_desc) => {
-            std::fs::write("openapi.yml", yaml_desc).unwrap();
+            std::fs::write(path, yaml_desc).unwrap();
         }
         Err(err) => {
             panic!("{:?}", err);
