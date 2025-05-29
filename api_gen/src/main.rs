e69b28ab706cdeaac89658479f1ab31cb8627800
Florin <florin.diaconeasa@gmail.com>
2025-05-29 13:13:13 +0300

    try out caching

--- api_gen/src/main.rs
+++ api_gen/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use utoipa::OpenApi;
 
 fn main() {
-    let path = env::args().nth(1).expect("Missing path argument");
+    let path = env::args().nth(1).expect("Missing path argument for openapi spec file.");
     let open_api = web_server::api::ApiDoc::openapi();
     match open_api.to_yaml() {
         Ok(yaml_desc) => {
