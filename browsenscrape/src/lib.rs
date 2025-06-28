b764152dbd8a0d9db9475bb177e52d6d79fb82db
Florin <florin.diaconeasa@gmail.com>
2025-06-28 22:34:23 +0300

    use ConnectionLike trait for testing

--- browsenscrape/src/lib.rs
+++ browsenscrape/src/lib.rs
@@ -1,18 +1,19 @@
 pub mod redis_store {
-    use common::Record;
-    use log::error;
-    use redis::{Commands, RedisError};
     pub use common::persistence::generate_redis_key;
     use common::persistence::SORTED_INCIDENTS_KEY;
+    use common::Record;
+    use log::error;
+    use redis::{cmd, RedisError};
 
-    pub fn store_record(incident: &Record, conn: &mut redis::Connection) -> Result<i32, String> {
+    pub fn store_record(incident: &Record, conn: &mut dyn redis::ConnectionLike) -> Result<i32, String> {
         let ser_inc = match serde_json::to_string(&incident) {
             Err(e) => return Err(e.to_string()),
             Ok(ser_res) => ser_res,
         };
 
         let new_key = generate_redis_key(&incident.id);
-        let redis_result: Result<String, RedisError> = conn.set(&new_key, ser_inc);
+
+        let redis_result: Result<String, RedisError> = cmd("SET").arg(&new_key).arg(ser_inc).query(conn);
 
         match redis_result {
             Err(e) => {
@@ -21,7 +22,12 @@ pub mod redis_store {
             }
             Ok(_rr) => {
                 let timestamp: &i64 = &incident.date.and_hms_opt(0, 0, 0).unwrap().and_utc().timestamp();
-                let new_elements: Result<i32, RedisError> = conn.zadd(SORTED_INCIDENTS_KEY, &new_key, timestamp);
+
+                let new_elements: Result<i32, RedisError> = cmd("ZADD")
+                    .arg(SORTED_INCIDENTS_KEY)
+                    .arg(&new_key)
+                    .arg(timestamp)
+                    .query(conn);
 
                 match new_elements {
                     Err(e) => {
