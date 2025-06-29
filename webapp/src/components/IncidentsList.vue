c41adc00f977840d81cc0b251ee6deb4e82f4741
Florin <florin.diaconeasa@gmail.com>
2025-06-29 15:19:15 +0300

    fix model on the client side

--- webapp/src/components/IncidentsList.vue
+++ webapp/src/components/IncidentsList.vue
@@ -57,7 +57,7 @@ const searchIncidentsByCounty = async (countyName?: string) => {
         type="radio"
         name="options"
         aria-label="1"
-        checked="checked" />
+    />
     <input class="join-item btn btn-square" type="radio" name="options" aria-label="2" />
     <input class="join-item btn btn-square" type="radio" name="options" aria-label="3" />
     <input class="join-item btn btn-square" type="radio" name="options" aria-label="4" />
