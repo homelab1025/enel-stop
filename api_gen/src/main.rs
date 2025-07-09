9f6af2f76e67e117c4a767c2c2e73b21f02b10d1
Florin <florin.diaconeasa@gmail.com>
2025-07-09 22:51:06 +0300

    WIP: have the python script run the browser and retrieve the RSS and post it to the web_server application

--- api_gen/src/main.rs
+++ api_gen/src/main.rs
@@ -3,7 +3,7 @@ use utoipa::OpenApi;
 
 fn main() {
     let path = env::args().nth(1).expect("Missing path argument for openapi spec file.");
-    let open_api = web_server::api::ApiDoc::openapi();
+    let open_api = web_server::web_api::ApiDoc::openapi();
     match open_api.to_yaml() {
         Ok(yaml_desc) => {
             std::fs::write(path, yaml_desc).unwrap();
