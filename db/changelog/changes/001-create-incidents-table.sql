def665b22120ba4ba16619ff3fcea03a92a4078d
Florin <florin.diaconeasa@gmail.com>
2025-07-14 15:57:47 +0300

    switch to yaml

--- db/changelog/changes/001-create-incidents-table.sql
+++ db/changelog/changes/001-create-incidents-table.sql
@@ -3,8 +3,6 @@
 --changeset author:initial-schema id:001
 --comment: Create incidents table based on the Incident struct
 
---"{\"id\":\"132587 - Retele Electrice\",\"date\":\"2025-06-17\",\"judet\":\"CONSTANTA\",\"localitate\":\"CONSTANTA\",\"title\":\"17.06.2025 09:00 - 12:00  Judet: CONSTANTA Localitate: CONSTANTA\",\"description\":\"Strada: ZONA DEDEMAN,  CU BLOC LOCUINTE PERPETUM RESIDENCE III - STRADA DOBRICH NR. 10 - INTRERUPERE TOTALA.\\\\t Numar:\"}"
-
 CREATE TABLE incidents
 (
     id          VARCHAR(255) PRIMARY KEY,
--- db/changelog/db.changelog-master.sql
+++
@@ -1,5 +0,0 @@
--- Liquibase Master Changelog
--- This file includes all the individual changelog files
-
--- Include the initial schema creation
-include db/changelog/changes/001-create-incidents-table.sql
