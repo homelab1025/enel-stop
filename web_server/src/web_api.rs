ec536b62883d11a6caf060af897132177694292f
Florin <florin.diaconeasa@gmail.com>
2025-07-31 21:31:20 +0300

    always order by day desc

--- web_server/src/web_api.rs
+++ web_server/src/web_api.rs
@@ -112,6 +112,8 @@ where
         query_builder.push(" OFFSET ").push_bind(offset as i64);
     }
 
+    query_builder.push(" ORDER BY day DESC");
+
     let incidents_query_result: Result<Vec<Incident>, Error> =
         query_builder.build_query_as().fetch_all(state.pg_pool.deref()).await;
 
